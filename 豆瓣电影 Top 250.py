30e90576c58cffc6d49f5ba511bd578894d2dd92
Pride <332842890@qq.com>
2020-02-09 00:14:20 +0800

    爬虫

--- 豆瓣电影 Top 250.py
+++ 豆瓣电影 Top 250.py
@@ -5,23 +5,6 @@ from lxml import etree
 from multiprocessing.dummy import Pool as ThreadPool
 import os
 
-headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36"}
-#保存爬取的信息
-data_title = [[] for _ in range(10)]
-data_score = [[] for _ in range(10)]
-data_address = [[] for _ in range(10)]
-#目录下没有img文件则创建此文件
-if not os.path.exists("img"):
-    os.makedirs("img")
-#开启10个进程分别爬取10个页面
-pool = ThreadPool(10)
-print("爬取开始...")
-pool.map(spider, range(10))
-pool.close()
-pool.join()
-save()
-print("爬取完成...")
-
 def spider(page):
     url = "https://movie.douban.com/top250?start={}&filter=".format(page * 25)
     text = requests.get(url, headers = headers).content.decode()
@@ -42,6 +25,21 @@ def save():
         for i in range(250):
             f.write("{}\t{}\t{}\t{}\n".format(i+1, data_title[i//25][i%25], data_score[i//25][i%25], data_address[i//25][i%25]))
 
-            
+headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36"}
+#保存爬取的信息
+data_title = [[] for _ in range(10)]
+data_score = [[] for _ in range(10)]
+data_address = [[] for _ in range(10)]
+#目录下没有img文件则创建此文件
+if not os.path.exists("img"):
+    os.makedirs("img")
+#开启10个进程分别爬取10个页面
+pool = ThreadPool(10)
+print("爬取开始...")
+pool.map(spider, range(10))
+pool.close()
+pool.join()
+save()
+print("爬取完成...")          
 
 
